af6de5d5377da3d6a780784845c48d7506f3b156
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-08-13 20:29:10 +0300

    loop work

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -9,16 +9,17 @@
 (defrecord Node [value child left right])
 (def tree {0 (Node. "Root" 1 nil nil)
            1 (Node. "B" nil 2 3)
-           2 (Node. "A" nil nil nil)
+           4 (Node. "A" nil nil nil)
            3 (Node. "C" nil nil nil)})
 (def nv "D")
-;(defn insert [tree root-id value])
+(def new-node (map->Node {:value "D"}))
 
 (loop [root 1]
   (if (nil? root)
-    (inc  42)
+    (let [new-key (inc (last (sort (keys tree))))]
+      (assoc tree  new-key new-node))
     (let [{:keys [value left right]} (tree root)
-          cmp-res (compare nv value)]
+          cmp-res (compare (:value new-node) value)]
       (cond
         (< cmp-res 0) (recur left)
         (> cmp-res 0) (recur right)
