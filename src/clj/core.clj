7404ab86e241ae0e5aeedaaae0e0fbe44f188922
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-08-16 23:14:23 +0300

    rename and small ref

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -7,10 +7,10 @@
   (println "Hello, World!"))
 
 (defrecord Node [value child left right])
-(def tree {0 (Node. "Root" 1 nil nil)
-           1 (Node. "B" nil 2 3)
-           2 (Node. "A" nil nil nil)
-           3 (Node. "C" nil nil nil)})
+(def directory {0 (->Node "Root" 1 nil nil)
+                1 (->Node "B" nil 2 3)
+                2 (->Node "A" nil nil nil)
+                3 (->Node "C" nil nil nil)})
 (def nv "D")
 ;(def new-node (map->Node {:value "D"}))
 
@@ -19,11 +19,11 @@
          parent-id nil
          direction nil]
     (if (nil? root-id)
-      (let [new-key (inc (last (sort (keys tree))))
+      (let [new-key (inc (last (sort (keys directory))))
             new-node (map->Node {:value nv})
-            updated-parent-node (assoc (tree parent-id) direction new-key)]
-        (assoc (assoc tree parent-id updated-parent-node) new-key new-node))
-      (let [{:keys [value left right]} (tree root-id)
+            updated-parent-node (assoc (directory parent-id) direction new-key)]
+        (assoc (assoc directory parent-id updated-parent-node) new-key new-node))
+      (let [{:keys [value left right]} (directory root-id)
             cmp-res (compare nv value)]
         (cond
           (< cmp-res 0) (recur left root-id :left)
