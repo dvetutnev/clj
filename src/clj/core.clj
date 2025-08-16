91b7551d4c7ada741a2b10285dcfdbfee64ac39a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-08-16 23:27:19 +0300

    directory map -> vector

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -7,21 +7,20 @@
   (println "Hello, World!"))
 
 (defrecord Node [value child left right])
-(def directory {0 (->Node "Root" 1 nil nil)
-                1 (->Node "B" nil 2 3)
-                2 (->Node "A" nil nil nil)
-                3 (->Node "C" nil nil nil)})
+(def directory [(->Node "Root" 1 nil nil) ;0
+                (->Node "B" nil 2 3)      ;1
+                (->Node "A" nil nil nil)  ;2
+                (->Node "C" nil nil nil)]);3
 (def nv "D")
-;(def new-node (map->Node {:value "D"}))
 
 (defn insert-in-tree []
   (loop [root-id 1
          parent-id nil
          direction nil]
     (if (nil? root-id)
-      (let [new-key (inc (last (sort (keys directory))))
+      (let [new-key (count directory)
             new-node (map->Node {:value nv})
-            updated-parent-node (assoc (directory parent-id) direction new-key)]
+            updated-parent-node (assoc (nth directory parent-id) direction new-key)]
         (assoc (assoc directory parent-id updated-parent-node) new-key new-node))
       (let [{:keys [value left right]} (directory root-id)
             cmp-res (compare nv value)]
