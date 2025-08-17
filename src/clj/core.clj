740163cfe7ea9bd03831cf8eefdf15493b3a3921
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-08-17 23:49:22 +0300

    reduce

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -6,11 +6,14 @@
   [& args]
   (println "Hello, World!"))
 
+(defrecord Node [value child left right])
+
 (defn add-node [directory parent-id direction node]
   (let [new-id (count directory)
         parent-node (nth directory parent-id)
         upd-parent-node (assoc parent-node direction new-id)]
-    (conj (assoc directory parent-id upd-parent-node) node)))
+    [(conj (assoc directory parent-id upd-parent-node) node)
+     new-id]))
 
 (defn insert-in-tree [directory root-id nv]
   (loop [root-id root-id
@@ -34,7 +37,6 @@
         (add-node directory storage-id :child new-node))
       (insert-in-tree directory root-id nv))))
 
-(defrecord Node [value child left right])
 (def directory [(->Node "Root" 1 nil nil) ;0
                 (->Node "B" nil 2 3)      ;1
                 (->Node "A" nil nil nil)  ;2
@@ -42,5 +44,16 @@
 (def nv "D")
 
 (def edir [(->Node "Root" nil nil nil)])
-(def edirc (insert-in-storage edir 0 nv))
+(def _edirc (insert-in-storage edir 0 nv))
+(def edirc (nth _edirc 0))
+
 (def nv2 "B")
+
+(defn fred [dir storage-id nv]
+  [(str dir nv)
+   (inc storage-id)])
+
+(defn add-path [path]
+  (reduce (fn [[directory storage-id] path-comp]
+            (fred directory storage-id path-comp))
+          ["Root" 42] path))
