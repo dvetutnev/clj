e5b781f4aef100414bb8825d377fe4ceb7aabf5d
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-08-18 16:46:37 +0300

    rename Node value -> name
    Node type
    
    fix Node value -> name

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -6,7 +6,7 @@
   [& args]
   (println "Hello, World!"))
 
-(defrecord Node [value child left right])
+(defrecord Node [name child left right type])
 
 (defn add-node [directory parent-id direction node]
   (let [new-id (count directory)
@@ -15,39 +15,37 @@
     [(conj (assoc directory parent-id upd-parent-node) node)
      new-id]))
 
-(defn insert-in-tree [directory root-id nv]
+(defn insert-in-tree [directory root-id node]
   (loop [root-id root-id
          parent-id nil
          direction nil]
     (if (nil? root-id)
-      (let [new-node (map->Node {:value nv})]
-        (add-node directory parent-id direction new-node))
-      (let [{:keys [value left right]} (directory root-id)
-            cmp-res (compare nv value)]
+      (add-node directory parent-id direction node)
+      (let [{:keys [name left right]} (directory root-id)
+            cmp-res (compare (:name node) name)]
         (cond
           (< cmp-res 0) (recur left root-id :left)
           (> cmp-res 0) (recur right root-id :right)
           :else nil)))))
 
-(defn insert-in-storage [directory storage-id nv]
+(defn insert-in-storage [directory storage-id node]
   (let [storage (nth directory storage-id)
         root-id (:child storage)]
     (if (nil? root-id)
-      (let [new-node (map->Node {:value nv})]
-        (add-node directory storage-id :child new-node))
-      (insert-in-tree directory root-id nv))))
+      (add-node directory storage-id :child node)
+      (insert-in-tree directory root-id node))))
 
-(def directory [(->Node "Root" 1 nil nil) ;0
-                (->Node "B" nil 2 3)      ;1
-                (->Node "A" nil nil nil)  ;2
-                (->Node "C" nil nil nil)]);3
-(def nv "D")
+(def directory [(map->Node {:name "Root" :type :storage :child 1})      ;0
+                (map->Node {:name "B" :type :storage :left 2 :right 3}) ;1
+                (map->Node {:name "A" :type :storage})                  ;2
+                (map->Node {:name "C" :type :storage})])                ;3
+(def nn (map->Node {:name "D" :type :storage}))
 
-(def edir [(->Node "Root" nil nil nil)])
-(def _edirc (insert-in-storage edir 0 nv))
+(def edir [(map->Node {:name "Root" :type :storage})])
+(def _edirc (insert-in-storage edir 0 nn))
 (def edirc (nth _edirc 0))
 
-(def nv2 "B")
+(def nn2 (map->Node {:name "B" :type :storage}))
 
 (defn fred [dir storage-id nv]
   [(str dir nv)
