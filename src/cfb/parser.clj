c35fea381c1a531004923b4fdbcbea807d0fc13c
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-09-11 23:49:32 +0300

    parser, open FileChannel

--- src/cfb/parser.clj
+++ src/cfb/parser.clj
@@ -1,4 +1,10 @@
 (ns cfb.parser
-  (:import (java.nio.file Paths)
+  (:import (java.nio.file Paths StandardOpenOption OpenOption)
            (java.nio.channels FileChannel)
            (java.nio ByteBuffer ByteOrder)))
+
+(def SectorSize 512)
+6
+(defn open-cfb [^String path]
+  (let [p (Paths/get path (into-array String []))
+        f (FileChannel/open p (into-array OpenOption [StandardOpenOption/READ]))] f))
