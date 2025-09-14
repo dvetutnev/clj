a48af091ee6668b6cd1793546406d23c65cc10cc
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-09-14 22:46:54 +0300

    read-header (uncomplete)

--- src/cfb/parser.clj
+++ src/cfb/parser.clj
@@ -4,7 +4,19 @@
            (java.nio ByteBuffer ByteOrder)))
 
 (def SectorSize 512)
-6
+
+(defn read-header [f]
+  (let [buffer (ByteBuffer/allocate SectorSize)
+        header (byte-array 8)]
+    (.order buffer ByteOrder/LITTLE_ENDIAN)
+    (.read f buffer)
+    (.rewind buffer)
+    {:minor-version (do
+                      (.get buffer header)
+                      (.get buffer (byte-array 16)) ; CLSID
+                      (.getShort buffer))}))
+
 (defn open-cfb [^String path]
   (let [p (Paths/get path (into-array String []))
-        f (FileChannel/open p (into-array OpenOption [StandardOpenOption/READ]))] f))
+        f (FileChannel/open p (into-array OpenOption [StandardOpenOption/READ]))]
+    (read-header f)))
