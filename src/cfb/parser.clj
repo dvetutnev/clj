5292077b89079c4f99288cd5572d796eeb079690
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-09-19 19:01:08 +0300

    read-header (more 8 items in hash-map)

--- src/cfb/parser.clj
+++ src/cfb/parser.clj
@@ -15,10 +15,25 @@
     (assert (java.util.Arrays/equals signature
                                      (byte-array [0xD0 0xCF 0x11 0xE0 0xA1 0xB1 0x1A 0xE1])))
     (.position buffer (+ (.position buffer) 16)) ; Skip CLSID
-    (let [header {:minor-version (.getShort buffer)
-                  :major-version (.getShort buffer)
-                  :byte-order (.getShort buffer)}]
+    (let [header (apply hash-map [:minor-version (.getShort buffer)
+                                  :major-version (.getShort buffer)
+                                  :byte-order (.getShort buffer)
+                                  :sector-shift (.getShort buffer)
+                                  :mini-sector-shift (.getShort buffer)
+                                  :num-fat-sector (do ; skip reserved and numdirectory sector
+                                                    (.position buffer (+ (.position buffer) 10))
+                                                    (.getInt buffer))
+                                  :start-directory (.getInt buffer)
+                                  :mini-stream-cutoff (do ; skip transaction signature
+                                                        (.position buffer (+ (.position buffer) 4))
+                                                        (.getInt buffer))
+                                  :start-minifat (.getInt buffer)])]
+
       (assert (= (:byte-order header) (unchecked-short 0xFFFE)))
+      (assert (= (:minor-version header) 0x003E))
+      (assert (= (:major-version header) 0x0003))
+      (assert (= (:sector-shift header) 0x0009))
+      #_(assert (= (:mini-sector-shift header) 0x0006))
       header)))
 
 (defn open-cfb [^String path]
