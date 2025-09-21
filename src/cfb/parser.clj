6d3a148acc746f142400c3072925b41a45472e19
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-09-21 21:18:12 +0300

    header shift-position

--- src/cfb/parser.clj
+++ src/cfb/parser.clj
@@ -4,6 +4,7 @@
            (java.nio ByteBuffer ByteOrder)))
 
 (def SectorSize 512)
+(def HeaderSize 512)
 
 (defn read-u16 [^ByteBuffer buffer]
   (-> buffer
@@ -15,8 +16,11 @@
       .getInt
       (bit-and 0xFFFFFFFF)))
 
+(defn shift-position [^ByteBuffer buffer n]
+  (.position buffer (+ (.position buffer) n)))
+
 (defn read-header [f]
-  (let [buffer (ByteBuffer/allocate SectorSize)
+  (let [buffer (ByteBuffer/allocate HeaderSize)
         signature (byte-array 8)]
     (.order buffer ByteOrder/LITTLE_ENDIAN)
     (.read f buffer)
@@ -24,18 +28,18 @@
     (.get buffer signature)
     (assert (java.util.Arrays/equals signature
                                      (byte-array [0xD0 0xCF 0x11 0xE0 0xA1 0xB1 0x1A 0xE1])))
-    (.position buffer (+ (.position buffer) 16)) ; Skip CLSID
+    (shift-position buffer 16) ; Skip CLSID
     (let [header (apply hash-map [:minor-version (read-u16 buffer)
                                   :major-version (read-u16 buffer)
                                   :byte-order (read-u16 buffer)
                                   :sector-shift (read-u16 buffer)
                                   :mini-sector-shift (read-u16 buffer)
                                   :num-fat-sector (do ; skip reserved and numdirectory sector
-                                                    (.position buffer (+ (.position buffer) 10))
+                                                    (shift-position buffer 10)
                                                     (read-u32 buffer))
                                   :start-directory-sector (read-u32 buffer)
                                   :mini-stream-cutoff (do ; skip transaction signature
-                                                        (.position buffer (+ (.position buffer) 4))
+                                                        (shift-position buffer 4)
                                                         (read-u32 buffer))
                                   :start-minifat (read-u32 buffer)
                                   :num-minifat-sector (read-u32 buffer)
