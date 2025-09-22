2f0b1e8a07ad103385bef10e0dbff1ae144daf5a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-09-23 01:18:56 +0300

    rename

--- src/cfb/parser.clj
+++ src/cfb/parser.clj
@@ -6,17 +6,17 @@
 (def SectorSize 512)
 (def HeaderSize 512)
 
-(defn read-u16 [^ByteBuffer buffer]
+(defn read-u16! [^ByteBuffer buffer]
   (-> buffer
       .getShort
       (bit-and 0xFFFF)))
 
-(defn read-u32 [^ByteBuffer buffer]
+(defn read-u32! [^ByteBuffer buffer]
   (-> buffer
       .getInt
       (bit-and 0xFFFFFFFF)))
 
-(defn shift-position [^ByteBuffer buffer n]
+(defn shift-position! [^ByteBuffer buffer n]
   (.position buffer (+ (.position buffer) n)))
 
 (defn read-header [f]
@@ -29,27 +29,27 @@
       (.get signature))
     (assert (java.util.Arrays/equals signature
                                      (byte-array [0xD0 0xCF 0x11 0xE0 0xA1 0xB1 0x1A 0xE1])))
-    (shift-position buffer 16) ; Skip CLSID
-    (let [header (apply hash-map [:minor-version (read-u16 buffer)
-                                  :major-version (read-u16 buffer)
-                                  :byte-order (read-u16 buffer)
-                                  :sector-shift (read-u16 buffer)
-                                  :mini-sector-shift (read-u16 buffer)
+    (shift-position! buffer 16) ; Skip CLSID
+    (let [header (apply hash-map [:minor-version (read-u16! buffer)
+                                  :major-version (read-u16! buffer)
+                                  :byte-order (read-u16! buffer)
+                                  :sector-shift (read-u16! buffer)
+                                  :mini-sector-shift (read-u16! buffer)
                                   :num-fat-sector (do ; skip reserved and numdirectory sector
-                                                    (shift-position buffer 10)
-                                                    (read-u32 buffer))
-                                  :start-directory-sector (read-u32 buffer)
+                                                    (shift-position! buffer 10)
+                                                    (read-u32! buffer))
+                                  :start-directory-sector (read-u32! buffer)
                                   :mini-stream-cutoff (do ; skip transaction signature
-                                                        (shift-position buffer 4)
-                                                        (read-u32 buffer))
-                                  :start-minifat (read-u32 buffer)
-                                  :num-minifat-sector (read-u32 buffer)
-                                  :start-difat-sector (read-u32 buffer)
-                                  :num-difat-sector (read-u32 buffer)])
+                                                        (shift-position! buffer 4)
+                                                        (read-u32! buffer))
+                                  :start-minifat (read-u32! buffer)
+                                  :num-minifat-sector (read-u32! buffer)
+                                  :start-difat-sector (read-u32! buffer)
+                                  :num-difat-sector (read-u32! buffer)])
 
           difat (let [difat (transient [])]
                   (doseq [_ (range (min (:num-fat-sector header) 109))]
-                    (conj! difat (read-u32 buffer)))
+                    (conj! difat (read-u32! buffer)))
                   (persistent! difat))]
 
       (assert (= (:minor-version header) 0x003E))
